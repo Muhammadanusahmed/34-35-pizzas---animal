8d0373d51682bedc3d80b87ed7ffead88075014f
MuammadAnusAhmed <MUHAMMADANUSAHMEDON@gmail.com>
2024-04-26 20:25:02 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+Question34: pizza: var pizzas = ["pepperoni", "margherita", "hawaiian"];
+pizzas.forEach(function (pizza) {
+    console.log("I like ".concat(pizza, " pizza."));
+});
+console.log("I really love pizza!");
+Question35: animal: var animals = ["dog", "cat", "rabbit"];
+animals.forEach(function (animal) {
+    console.log("A ".concat(animal, " would make a great pet."));
+});
+console.log("Any of these animals would make a great pet!");
---
+++ app.ts
@@ -0,0 +1,18 @@
+Question34: pizza:
+
+let pizzas: string[] = ["pepperoni", "margherita", "hawaiian"]
+
+pizzas.forEach(pizza => {
+    console.log(`I like ${pizza} pizza.`);
+});
+console.log("I really love pizza!");
+
+Question35: animal:
+
+let animals: string[] = ["dog", "cat", "rabbit"];
+
+animals.forEach(animal => {
+    console.log(`A ${animal} would make a great pet.`);
+});
+console.log("Any of these animals would make a great pet!");
+
